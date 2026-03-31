158fa1620f892e7387deb6a79da2fa3a3fb84775
Riski153 <m.riski.f21@mail.umy.ac.id>
2026-03-31 12:17:43 +0700

    tambahkan fungsi input

--- main.cpp
+++ main.cpp
@@ -1,2 +1,9 @@
 #include <iostream>
 using namespace std;
+
+// Prosedur untuk input
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
